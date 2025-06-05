8df3e527a4be106929d822619ff184836ddbe69d
ppanagiotisilv <ppanagiotisilv@gmail.com>
2025-06-05 07:52:34 +0000

    initial files

---
+++ .idx/dev.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodejs_20
+  ];
+
+  # Sets environment variables in the workspace
+  env = { };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "astro-build.astro-vscode"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [ 
+            "npm"
+            "run"
+            "start"
+            "--"
+            "--port"
+            "$PORT"
+            "--host"
+            "0.0.0.0" ];
+          manager = "web";
+        };
+      };
+    };
+  };
+}
+  
