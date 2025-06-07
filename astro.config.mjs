76b29d8479520a3ca080453ca65cd68fea4cf43e
ppanagiotisilv <ppanagiotisilv@gmail.com>
2025-06-07 11:28:42 +0000

    nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://panugr.github.io',
-    base: 'mikegeorgiou',
+    base: '/mikegeorgiou',
     prefetch: true,
     trailingSlash:"never"
 });
