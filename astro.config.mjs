994486a0c1461c17930d33e774bac51e0de40a31
Panagiotis Panagiotakopoulos <64307674+PanuGr@users.noreply.github.com>
2025-06-07 13:31:46 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://panugr.github.io/mikegeorgiou/',
+    site: 'https://panugr.github.io',
     base: 'my-repo',
     prefetch: true,
     trailingSlash:"never"
